cbacecf7637d3dac199221f88b1235c3ea318665
hwncito <juanchosperoni@hotmail.com>
2024-03-06 09:15:41 -0300

    Modified the logo of ifcares in the auth section

--- src/app/auth/login/page.jsx
+++ src/app/auth/login/page.jsx
@@ -65,8 +65,8 @@ export default function Login() {
   return (
     <div className="bg-gray-50 dark:bg-gray-900">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <div className="flex items-center mb-6 text-2xl font-semibold text-gray-900 rounded-xl shadow-xl">
-          <Image src="/UpdatedLogo.jpg" alt="logo" width={100} height={100} />
+        <div className="flex items-center mb-5 text-2xl font-semibold text-gray-900">
+          <Image src="/web-logo.png" alt="logo" width={200} height={200} />
         </div>
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
