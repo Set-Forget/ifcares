09ce801c9d1c969a0a7c73b6869c701c5049328f
hwncito <juanchosperoni@hotmail.com>
2024-10-04 18:00:19 -0300

    modified the webapp url. Now add, delete and edit functions work way faster

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,4 +5,6 @@ export const ROLES = {
 
 // export const API_BASE_URL = 'https://script.google.com/macros/s/AKfycbxJSL4ZXwPfBotAI6x-BfcYz9B-6yRsadamRPWLmkkMIeZkC6ZLMiPMzxvSBobL0wsH/exec'
 
-export const API_BASE_URL = 'https://script.google.com/macros/s/AKfycbysKN5zR8iUZce8LIBhX14BIJXOhFTWw-ZcX5NuPLhXsnN7-jWTkAQUV12dkrRdDj9l/exec'
+// export const API_BASE_URL = 'https://script.google.com/macros/s/AKfycbysKN5zR8iUZce8LIBhX14BIJXOhFTWw-ZcX5NuPLhXsnN7-jWTkAQUV12dkrRdDj9l/exec'
+
+export const API_BASE_URL = 'https://script.google.com/macros/s/AKfycbwPwKePlcflGz1UcrS3-o7nssAoC0BGSPr8rLuxxjq2nePnWghXlJYI4Bsdvksur3tS/exec'
